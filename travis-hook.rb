7c1d71696de493980d47ed2232937e325c12275b
Parker Moore <parkrmoore@gmail.com>
2014-07-27 00:13:02 -0400

    Separate workers.

--- travis-hook.rb
+++ travis-hook.rb
@@ -1,55 +1,11 @@
 require 'json'
 require 'logger'
-require 'open3'
-require 'sidekiq'
+require './workers/hook'
 require 'sinatra'
 require 'yaml'
 
 repos    = YAML.load_file("config/repos.yml")
-log_file = "log/production.log"
-logger   = Logger.new(log_file)
-
-class HookWorker
-  include Sidekiq::Worker
-
-  def exec(*cmd)
-    logger.info "CWD=#{Dir.pwd} : Executing '#{cmd.join(" ")}'..."
-    Open3.popen2e(*cmd) do |stdin, stdout_and_stderr, wait_thr|
-      exit_status = wait_thr.value.exitstatus
-      output = stdout_and_stderr.read
-      logger.info output
-      [exit_status, output]
-    end
-  end
-
-  def clone(clone_url, destination)
-    # clone
-    exec("git", "clone", clone_url, destination)
-    # update
-    Dir.chdir(destination) do
-      exec("git", "pull", "origin")
-    end
-  end
-
-  def make_new_commit(output_dir, commit_url)
-    # commit and push
-    Dir.chdir(output_dir) do
-      exec(
-        "git",
-        "commit",
-        "--allow-empty",
-        "-m",
-        "Building commit corresponding to #{commit_url}"
-      )
-      exec("git", "push")
-    end
-  end
-
-  def perform(clone_url, output_dir, commit_url)
-    clone(clone_url, output_dir)
-    make_new_commit(output_dir, commit_url)
-  end
-end
+logger   = Logger.new("log/production.log")
 
 def fetch_data(request)
   request.body.rewind
---
+++ workers/hook.rb
@@ -0,0 +1,47 @@
+require 'logger'
+require 'open3'
+require 'sinatra'
+
+logger = Logger.new("log/production.log")
+
+class HookWorker
+  include Sidekiq::Worker
+
+  def exec(*cmd)
+    logger.info "CWD=#{Dir.pwd} : Executing '#{cmd.join(" ")}'..."
+    Open3.popen2e(*cmd) do |stdin, stdout_and_stderr, wait_thr|
+      exit_status = wait_thr.value.exitstatus
+      output = stdout_and_stderr.read
+      logger.info output
+      [exit_status, output]
+    end
+  end
+
+  def clone(clone_url, destination)
+    # clone
+    exec("git", "clone", clone_url, destination)
+    # update
+    Dir.chdir(destination) do
+      exec("git", "pull", "origin")
+    end
+  end
+
+  def make_new_commit(output_dir, commit_url)
+    # commit and push
+    Dir.chdir(output_dir) do
+      exec(
+        "git",
+        "commit",
+        "--allow-empty",
+        "-m",
+        "Building commit corresponding to #{commit_url}"
+      )
+      exec("git", "push")
+    end
+  end
+
+  def perform(clone_url, output_dir, commit_url)
+    clone(clone_url, output_dir)
+    make_new_commit(output_dir, commit_url)
+  end
+end
