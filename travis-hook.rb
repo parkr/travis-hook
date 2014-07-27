3f4b193d8447f8d7d0113c9fd2ef24447757d5f6
Parker Moore <parkrmoore@gmail.com>
2014-07-27 15:43:26 -0400

    Do signatures right.

--- travis-hook.rb
+++ travis-hook.rb
@@ -5,8 +5,8 @@ require 'sinatra'
 require 'yaml'
 
 repos    = YAML.load_file("config/repos.yml")
-logger   = Logger.new("log/production.log")
 secrets  = YAML.load_file("config/secrets.yml")
+logger   = Logger.new("log/production.log")
 
 def request_body(request)
   request.body.rewind
@@ -21,9 +21,12 @@ def encoded_signature(token, payload_body)
   'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), token, payload_body)
 end
 
-def verify_signature(request)
+def verify_signature(request, secrets)
   sent_signature = request.env['HTTP_X_HUB_SIGNATURE']
-  logger.info "Verifying #{sent_signature} is a valid signature..."
+  if sent_signature.nil?
+    return halt 400, "Be sure to send the header X-Hub-Signature."
+  end
+  logger.info "Verifying '#{sent_signature}' is a valid signature..."
   payload_body = request_body(request)
   unless secrets.any? { |sig|
     Rack::Utils.secure_compare(encoded_signature(sig, payload_body), sent_signature)
@@ -40,7 +43,7 @@ end
 post '/_github' do
   data = fetch_data(request)
   logger.info data.to_s
-  verify_signature(request)
+  verify_signature(request, secrets)
   repos.map do |clone_url, rel_output_dir|
     output_dir = File.join("repos", rel_output_dir)
     commit_url = data.fetch('head_commit').fetch('url')
